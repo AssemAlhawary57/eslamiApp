56490be3eb8b0577383b23b62eb5b918e76e0ac0
Hawary <Assem.hawary57@gmail.com>
2022-12-02 07:26:59 -0600

    مسحت اول حاجه في محاضرة الجيت

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Xo());
+}
+
+class Xo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(children: [
+          Expanded(child: Text('Amir88888888ooooooa')),
+          Expanded(child: Text('ahmed')),
+        ]),
+      ),
+    );
+  }
+}
